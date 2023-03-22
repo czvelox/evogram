3ff0dc3c09c69eefbfea7740b569059a52b2312c
Велокс <czvelox@gmail.com>
2023-03-22 11:41:07 +0300

    Add token validation

--- src/Client.ts
+++ src/Client.ts
@@ -1,4 +1,5 @@
 import { API } from "./API";
+import { BotContext } from "./contexts";
 import { ContextManager } from "./modules/context";
 import { Updates } from "./updates";
 
@@ -15,7 +16,9 @@ export class Evogram {
 	public updates: Updates;
 	/** An instance of the ContextManager for managing and manipulating contexts. */
 	public contexts: ContextManager;
-
+	/** Context of a running bot */
+	public bot?: BotContext;
+ 
 	/**
 	 * Create a new instance of the Evogram class.
 	 * @param {IEvogramParams} options - The options for initializing the instance.
@@ -24,5 +27,21 @@ export class Evogram {
 		this.api = new API(this);
 		this.updates = new Updates(this);
 		this.contexts = new ContextManager(this);
+
+		this.checkTokenValidity();
+	}
+
+	/**
+	 * Check if a working access token is specified
+	 * @return A Promise BotContext instance
+	 * @throws Error if the specified token is invalid
+	 */
+	public async checkTokenValidity() {
+		try {
+			this.bot = await this.api.getMe();
+			return this.bot;
+		} catch {
+			throw new Error("The specified token is invalid");
+		}
 	}
 }
