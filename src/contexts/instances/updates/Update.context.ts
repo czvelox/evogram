de5f41f26795597512370a86c7c77ce8ad35873d
Ilya Frolov <czvelox@gmail.com>
2024-09-09 17:49:05 +0300

    fix: change names

--- src/contexts/instances/updates/Update.context.ts
+++ src/contexts/instances/updates/Update.context.ts
@@ -8,6 +8,7 @@ export class UpdateContext extends Context<TelegramUpdate> {
 		super(contextParams);
 		//@ts-ignore
 		if (this.serviceMessage?.name) this.name = 'service_message';
+		console.log(this.service_message);
 	}
 
 	/** The ID of the update. */
@@ -39,27 +40,27 @@ export class UpdateContext extends Context<TelegramUpdate> {
             this.source.removed_chat_boost        && "removed_chat_boost"         ;
 
 	public message = this.getContext({ key: 'Message', source: this.source.message });
-	public editedMessage = this.getContext({ key: 'Message', source: this.source.edited_message });
-	public channelPost = this.getContext({ key: 'Message', source: this.source.channel_post });
-	public editedChannelPost = this.getContext({ key: 'Message', source: this.source.edited_channel_post });
-	public businessConnection = this.getContext({ key: 'Message', source: this.source.business_connection });
-	public businessMessage = this.getContext({ key: 'Message', source: this.source.business_message });
-	public editedBusinessMessage = this.getContext({ key: 'Message', source: this.source.edited_business_message });
-	public deletedBusinessMessages = this.getContext({ key: 'Message', source: this.source.deleted_business_messages });
-	public messageReaction = this.getContext({ key: 'MessageReaction', source: this.source.message_reaction });
-	public messageReactionCount = this.getContext({ key: 'MessageReactionCount', source: this.source.message_reaction_count });
-	public inlineQuery = this.getContext({ key: 'InlineQuery', source: this.source.inline_query });
-	public chosenInlineResult = this.getContext({ key: 'ChosenInlineResult', source: this.source.chosen_inline_result });
-	public callbackQuery = this.getContext({ key: 'CallbackQuery', source: this.source.callback_query });
-	public shippingQuery = this.getContext({ key: 'ShippingQuery', source: this.source.shipping_query });
-	public preCheckoutQuery = this.getContext({ key: 'PreCheckoutQuery', source: this.source.pre_checkout_query });
+	public edited_message = this.getContext({ key: 'Message', source: this.source.edited_message });
+	public channel_post = this.getContext({ key: 'Message', source: this.source.channel_post });
+	public edited_channel_post = this.getContext({ key: 'Message', source: this.source.edited_channel_post });
+	public business_connection = this.getContext({ key: 'Message', source: this.source.business_connection });
+	public business_message = this.getContext({ key: 'Message', source: this.source.business_message });
+	public edited_business_message = this.getContext({ key: 'Message', source: this.source.edited_business_message });
+	public deleted_business_messages = this.getContext({ key: 'Message', source: this.source.deleted_business_messages });
+	public message_reaction = this.getContext({ key: 'MessageReaction', source: this.source.message_reaction });
+	public message_reaction_count = this.getContext({ key: 'MessageReactionCount', source: this.source.message_reaction_count });
+	public inline_query = this.getContext({ key: 'InlineQuery', source: this.source.inline_query });
+	public chosen_inline_result = this.getContext({ key: 'ChosenInlineResult', source: this.source.chosen_inline_result });
+	public callback_query = this.getContext({ key: 'CallbackQuery', source: this.source.callback_query });
+	public shipping_query = this.getContext({ key: 'ShippingQuery', source: this.source.shipping_query });
+	public pre_checkout_query = this.getContext({ key: 'PreCheckoutQuery', source: this.source.pre_checkout_query });
 	public poll = this.getContext<PollContext>({ key: 'Poll', source: this.source.poll });
-	public pollAnswer = this.getContext<PollAnswerContext>({ key: 'PollAnswer', source: this.source.poll_answer });
-	public myChatMember = this.getContext({ key: 'ChatMemberUpdated', source: this.source.my_chat_member });
-	public chatMember = this.getContext({ key: 'ChatMemberUpdated', source: this.source.chat_member });
-	public chatJoinRequest = this.getContext({ key: 'ChatJoinRequest', source: this.source.chat_join_request });
-	public chatBoost = this.getContext({ key: 'ChatBoost', source: this.source.chat_boost });
-	public removedChatBoost = this.getContext({ key: 'RemovedChatBoost', source: this.source.removed_chat_boost });
+	public poll_answer = this.getContext<PollAnswerContext>({ key: 'PollAnswer', source: this.source.poll_answer });
+	public my_chat_member = this.getContext({ key: 'ChatMemberUpdated', source: this.source.my_chat_member });
+	public chat_member = this.getContext({ key: 'ChatMemberUpdated', source: this.source.chat_member });
+	public chat_join_request = this.getContext({ key: 'ChatJoinRequest', source: this.source.chat_join_request });
+	public chat_boost = this.getContext({ key: 'ChatBoost', source: this.source.chat_boost });
+	public removed_chat_boost = this.getContext({ key: 'RemovedChatBoost', source: this.source.removed_chat_boost });
 
-	public serviceMessage = this.name === 'service_message' && this.getContext({ key: 'ServiceMessage', source: this.source.message });
+	public service_message = this.getContext({ key: 'ServiceMessage', source: this.source.message });
 }
