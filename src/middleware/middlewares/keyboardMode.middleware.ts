ad1ae9b30d3fab4ef0ae78a2962dba6b0c13355b
Ilya Frolov <czvelox@gmail.com>
2024-11-02 05:34:04 +0300

    fix: remove error log

--- src/middleware/middlewares/keyboardMode.middleware.ts
+++ src/middleware/middlewares/keyboardMode.middleware.ts
@@ -4,7 +4,7 @@ class KeyboardMiddleware {
 	@MiddlewareD()
 	async middleware(ctx: MiddlewareContext, next: any) {
 		if (!ctx.client.params.keyboardMode || !ctx.message || ctx.message?.chat.type !== 'private') return next();
-		setTimeout(() => ctx.client.api.deleteMessage({ chat_id: ctx.message!.chat.id, message_id: ctx.message!.message_id }), 5000);
+		setTimeout(() => ctx.client.api.deleteMessage({ chat_id: ctx.message!.chat.id, message_id: ctx.message!.message_id }).catch(() => {}), 5000);
 
 		return next();
 	}
